8e8cf1add6bade0658c4e39bb286736ee7ad70fa
Sudhan2001 <sudhangopi2001@gmail.com>
2022-03-05 18:08:16 +0530

    RET123

---
+++ src/org/manimaran/Magesh.java
@@ -0,0 +1,16 @@
+package org.manimaran;
+
+public class Magesh {
+	private void thameem() {
+	System.out.println("sudhan");
+	}
+	private void sangeetha() {
+		System.out.println("123");
+	}
+	public static void main(String[] args) {
+		Magesh s=new Magesh();
+	s.thameem();
+	s.sangeetha();
+	}
+
+}
